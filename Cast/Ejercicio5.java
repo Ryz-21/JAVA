00fade68b10c8be89beaa4d2931052f2bee5f804
leo valdivia suasnabar <darkltata@gmail.com>
2025-11-04 12:12:55 -0500

    Cast

---
+++ Cast/Ejercicio5.java
@@ -0,0 +1,14 @@
+package Cast;
+
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int  numero = 66;
+
+        char valor = (char) numero;
+
+        System.out.println("" + valor);
+        
+    }
+
+}
