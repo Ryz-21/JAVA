ace810963e752c09150f9edab995e0f9bb2f2a64
leo valdivia suasnabar <darkltata@gmail.com>
2025-11-07 12:16:08 -0500

    actualizacion

---
+++ Exception/Ejercicio7.java
@@ -0,0 +1,13 @@
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        try {
+            lanzarExcepcion();
+        } catch (Exception e) {
+            System.out.println("Se capturó la excepción: " + e.getMessage());
+        }
+    }
+
+    public static void lanzarExcepcion() {
+        throw new RuntimeException("¡Esta es una excepción personalizada lanzada con throw!");
+    }
+}
