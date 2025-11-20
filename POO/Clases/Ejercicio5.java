b191788f8ed2c8a34671571cf64a6e8d42b2036a
leo valdivia suasnabar <darkltata@gmail.com>
2025-11-20 16:38:45 -0500

    acabando ejercicios de clases

--- POO/Clases/Ejercicio5.java
+++ POO/Clases/Ejercicio5.java
@@ -1,3 +1,5 @@
+
+
 /*
  * EJERCICIO 5: Clase Rectángulo
  * 
@@ -13,12 +15,66 @@
  * En main, crea varios rectángulos y comprueba si son cuadrados,
  * calcula su área y perímetro.
  */
+class rectangulo {
+    double ancho;
+    double alto;
 
+    @Override
+    public String toString(){
+        return "Rectangulo [ancho=" + ancho + ", alto=" + alto + ", area=" + area() + ", perimetro=" + perímetro() + "]";
+    }
+    public double area (){
+        return ancho*alto;
+    }
+    public double perímetro (){
+        return 2 * (ancho*alto);
+    }
+    public boolean verificarCuadrado(){
+        return ancho == alto;
+    }
+
+    public rectangulo(double ancho, double alto) {
+        if(ancho <=0 || alto <= 0){
+            throw new IllegalArgumentException("el ancho y el alto debe ser mayor a 0");
+        }
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        if(alto <= 10){
+            throw new IllegalArgumentException("el ancho debe ser mayor a 0");
+        }
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        if(alto <= 10){
+            throw  new IllegalArgumentException("el alto debe ser mayor que 0");
+        }
+    }
+
+   
+    
+
+}
 public class Ejercicio5 {
     
     // TODO: Implementar aquí
     
     public static void main(String[] args) {
         // TODO: Crear objetos y probar los métodos
+        rectangulo numero1 = new rectangulo(20, 10);
+
+        numero1.area();
+        numero1.perímetro();
+        numero1.verificarCuadrado();
     }
 }
