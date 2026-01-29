46ad4d34be5d00e3bef945a03fbf6b2a7ff85154
Leo Suasnabar <Darkltata@gmail.com>
2026-01-29 14:24:26 -0500

    acabamos por fin java intermedio en conceptos basicos

--- Intermedio/Autoboxing y Unboxing/Ejercicio5.java
+++ Intermedio/Autoboxing y Unboxing/Ejercicio5.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Ejercicio5 {
     public static void main(String[] args) {
-        
+        // crear un ArrayList de Integer
+        ArrayList<Integer> numeros = new ArrayList<>();
+        // agregar 10 numeros enteros al ArrayList
+        for (int i = 0; i < 10; i++) {
+            numeros.add(i);
+        }
+        // recorrer el ArrayList y mostrar cada numero
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
     }
 }
